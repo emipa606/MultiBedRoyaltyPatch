8eb18e8b2a57f31937294b84d82ae7bdb61b5a7f
Mlie <emipa606@users.noreply.github.com>
2024-03-22 21:52:32 +0100

    Mod updated for 1.5 and passed autotests

--- Source/MultiBedRoyaltyPatch/MultiBedRoyaltyPatch.cs
+++ Source/MultiBedRoyaltyPatch/MultiBedRoyaltyPatch.cs
@@ -9,7 +9,6 @@ internal static class MultiBedRoyaltyPatch
 {
     static MultiBedRoyaltyPatch()
     {
-        Log.Message("Multi-Bed Royalty Patch started.");
         new Harmony("Multi-Bed Royalty Patch").PatchAll(Assembly.GetExecutingAssembly());
     }
 }
